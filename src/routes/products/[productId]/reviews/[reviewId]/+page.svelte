9787c9d9ca7c81e9ddd8f2e5d26d1f3fc4036e3d
halil180 <halilkom180@.com>
2023-02-12 11:33:33 +0100

    nested routes

---
+++ src/routes/products/[productId]/reviews/[reviewId]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from "$app/stores";
+
+
+    const reviewId = $page.params.reviewId
+    const productId = $page.params.productId
+</script>
+
+
+<h1>Review {reviewId} for the product with the id of    {productId}</h1>
